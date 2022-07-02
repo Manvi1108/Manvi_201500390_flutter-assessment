41e8d33512d9a12da2ee104b2715642092d61b58
Manvi1108 <manvisinghal285@gmail.com>
2022-07-02 15:01:06 +0530

    assessment

---
+++ flutter_asessment/lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_asessment/screen1.dart';
+import 'screen1.dart';
+import 'screen2.dart';
+import 'screen3.dart';
+
+void main()
+{
+  runApp(screen1());
+}
---
+++ flutter_asessment/lib/screen1.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+
+class screen1 extends StatefulWidget {
+  const screen1({Key? key}) : super(key: key);
+
+  @override
+  State<screen1> createState() => _screen1State();
+}
+
+class _screen1State extends State<screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("TRAINING",style: TextStyle(fontWeight: FontWeight.bold),),
+          backgroundColor: Color.fromARGB(212, 40, 130, 195)),
+        body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: 
+          [
+            Image(
+                image: AssetImage('images/training.png'),
+                ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Get Started" , style: TextStyle(color: Colors.blue)),
+            ),
+          ]
+        ))
+      ),
+    );
+    
+  }
+}
